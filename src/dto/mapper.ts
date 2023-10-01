9b5cd3b47b47d92a3fa91245457011bd79d04f92
petruki <31597636+petruki@users.noreply.github.com>
2023-10-01 15:30:36 -0700

    Replaced middleware by mapper for request build

--- src/dto/mapper.ts
+++ src/dto/mapper.ts
@@ -1,7 +1,9 @@
-import { Output } from '../deps.ts';
+import { Context, Output } from '../deps.ts';
+import { getBooleanParam, getParam } from '../utils.ts';
+import { SearchDocsQueryParams, SearchDocsRequestDto } from './request.ts';
 import { SearchDocsResponseDto } from './response.ts';
 
-export function toSearchDocsDto(output: Output[]): SearchDocsResponseDto {
+export function toSearchDocsResponseDto(output: Output[]): SearchDocsResponseDto {
   return {
     code: 200,
     message: 'success',
@@ -11,5 +13,22 @@ export function toSearchDocsDto(output: Output[]): SearchDocsResponseDto {
       found: item.found,
       cache: item.cache,
     })),
+  };
+}
+
+export function toSearchDocsRequestDto(context: Context): SearchDocsRequestDto {
+  const APP_FILES = Deno.env.get('APP_FILES') || '';
+  const APP_CONTEXT_ENDPOINT = Deno.env.get('APP_CONTEXT_ENDPOINT') || '';
+
+  const searchParams = context.request.url.searchParams;
+  return {
+    query: String(getParam(searchParams, SearchDocsQueryParams.query, '')),
+    url: String(getParam(searchParams, SearchDocsQueryParams.url, APP_CONTEXT_ENDPOINT)),
+    files: String(getParam(searchParams, SearchDocsQueryParams.files, APP_FILES)).split(','),
+    previewLength: parseInt(String(getParam(searchParams, SearchDocsQueryParams.previewLength, '0'))),
+    ignoreCase: getBooleanParam(searchParams, SearchDocsQueryParams.ignoreCase, false),
+    trimContent: getBooleanParam(searchParams, SearchDocsQueryParams.trimContent, false),
+    regex: getBooleanParam(searchParams, SearchDocsQueryParams.regex, false),
+    skipCache: getBooleanParam(searchParams, SearchDocsQueryParams.skipCache, false),
   };
 }
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,6 +1,5 @@
 import { bold, Context, cyan, green, Next } from '../deps.ts';
-import { SearchDocsQueryParams, SearchDocsRequestDto } from '../dto/request.ts';
-import { getBooleanParam, getParam, logger } from '../utils.ts';
+import { logger } from '../utils.ts';
 
 export const responseTime = async (context: Context, next: Next) => {
   const start = Date.now();
@@ -18,24 +17,4 @@ export const responseTimeLog = async (context: Context, next: Next) => {
     'Route',
     `${green(context.request.method)} ${cyan(decodeURIComponent(String(context.request.url)))} - ${bold(String(rt))}`,
   );
-};
-
-export const mapToSearchDocsRequestDto = async (context: Context, next: Next) => {
-  const APP_FILES = Deno.env.get('APP_FILES') || '';
-  const APP_CONTEXT_ENDPOINT = Deno.env.get('APP_CONTEXT_ENDPOINT') || '';
-
-  const searchParams = context.request.url.searchParams;
-  const request = {
-    query: getParam(searchParams, SearchDocsQueryParams.query, ''),
-    url: getParam(searchParams, SearchDocsQueryParams.url, APP_CONTEXT_ENDPOINT),
-    files: String(getParam(searchParams, SearchDocsQueryParams.files, APP_FILES)).split(','),
-    previewLength: parseInt(getParam(searchParams, SearchDocsQueryParams.previewLength, '0') as string),
-    ignoreCase: getBooleanParam(searchParams, SearchDocsQueryParams.ignoreCase, false),
-    trimContent: getBooleanParam(searchParams, SearchDocsQueryParams.trimContent, false),
-    regex: getBooleanParam(searchParams, SearchDocsQueryParams.regex, false),
-    skipCache: getBooleanParam(searchParams, SearchDocsQueryParams.skipCache, false),
-  } as SearchDocsRequestDto;
-
-  context.state.request = request;
-  await next();
 };
