46bd357a11e5226a602045747e04cc4716f33333
petruki <31597636+petruki@users.noreply.github.com>
2023-10-07 15:50:50 -0700

    Fixed service init - bumped skimming to 1.0.11

--- src/deps.ts
+++ src/deps.ts
@@ -3,5 +3,5 @@ export type { Next } from 'https://deno.land/x/oak@v12.6.1/mod.ts';
 export { Context } from 'https://deno.land/x/oak@v12.6.1/context.ts';
 export { load } from 'https://deno.land/std@0.203.0/dotenv/mod.ts';
 export { bold, cyan, green } from 'https://deno.land/std@0.203.0/fmt/colors.ts';
-export { Skimming } from 'https://deno.land/x/skimming@v1.0.9/mod.ts';
-export type { Output } from 'https://deno.land/x/skimming@v1.0.9/mod.ts';
+export { Skimming } from 'https://deno.land/x/skimming@v1.0.11/mod.ts';
+export type { Output } from 'https://deno.land/x/skimming@v1.0.11/mod.ts';
