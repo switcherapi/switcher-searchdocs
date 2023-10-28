7a6cf0ae9ac1013bc35eb061ddbeb311826c9b0c
petruki <31597636+petruki@users.noreply.github.com>
2023-10-28 14:05:06 -0700

    Fixed helmet to support preflight request

--- src/deps.ts
+++ src/deps.ts
@@ -1,7 +1,7 @@
 export { Application, Router } from 'https://deno.land/x/oak@v12.6.1/mod.ts';
 export type { Middleware, Next } from 'https://deno.land/x/oak@v12.6.1/mod.ts';
 export { Context } from 'https://deno.land/x/oak@v12.6.1/context.ts';
-export { load } from 'https://deno.land/std@0.203.0/dotenv/mod.ts';
-export { bold, cyan, green } from 'https://deno.land/std@0.203.0/fmt/colors.ts';
+export { load } from 'https://deno.land/std@0.204.0/dotenv/mod.ts';
+export { bold, cyan, green } from 'https://deno.land/std@0.204.0/fmt/colors.ts';
 export { Skimming } from 'https://deno.land/x/skimming@v1.0.11/mod.ts';
 export type { Output } from 'https://deno.land/x/skimming@v1.0.11/mod.ts';
