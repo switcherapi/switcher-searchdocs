8d36a0a4400dbe3fc91437bfd8f74bedfe07db50
Roger Floriano <31597636+petruki@users.noreply.github.com>
2023-12-12 19:58:18 -0800

    Bump Deno runtime to 1.38.5 - improved helmet - fixed CI actions (#16)

--- src/middleware/helmet.ts
+++ src/middleware/helmet.ts
@@ -1,22 +1,32 @@
 import { Context, Middleware, Next } from '../deps.ts';
 
+const SERVER = 'Deno';
+const ACCESS_CONTROL_ALLOW_ORIGIN = '*';
+const ACCESS_CONTROL_ALLOW_METHODS = 'GET,POST,OPTIONS';
+const ACCESS_CONTROL_ALLOW_HEADERS = 'Content-Type, Accept';
+const CONTENT_SECURITY_POLICY =
+  'default-src \'self\' \'unsafe-inline\' \'unsafe-eval\' data: https: http:; object-src \'none\'; base-uri \'none\'; form-action \'self\'; frame-ancestors \'none\';';
+const X_CONTENT_TYPE_OPTIONS = 'nosniff';
+const X_FRAME_OPTIONS = 'DENY';
+const X_XSS_PROTECTION = '1; mode=block';
+const STRICT_TRANSPORT_SECURITY = 'max-age=31536000; includeSubDomains; preload';
+const REFERRER_POLICY = 'no-referrer';
+const PERMISSIONS_POLICY = 'geolocation=(), microphone=(), camera=(), payment=()';
+
 export default class Helmet {
   public middleware(): Middleware {
     return async (context: Context, next: Next) => {
-      context.response.headers.set('Server', 'Deno');
-      context.response.headers.set('Access-Control-Allow-Origin', '*');
-      context.response.headers.set('Access-Control-Allow-Methods', 'GET,OPTIONS');
-      context.response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Accept');
-      context.response.headers.set(
-        'Content-Security-Policy',
-        'default-src \'self\' \'unsafe-inline\' \'unsafe-eval\' data: https: http:; object-src \'none\'; base-uri \'none\'; form-action \'self\'; frame-ancestors \'none\';',
-      );
-      context.response.headers.set('X-Content-Type-Options', 'nosniff');
-      context.response.headers.set('X-Frame-Options', 'DENY');
-      context.response.headers.set('X-XSS-Protection', '1; mode=block');
-      context.response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
-      context.response.headers.set('Referrer-Policy', 'no-referrer');
-      context.response.headers.set('Permissions-Policy', 'geolocation=(), microphone=(), camera=(), payment=()');
+      context.response.headers.set('Server', SERVER);
+      context.response.headers.set('Access-Control-Allow-Origin', ACCESS_CONTROL_ALLOW_ORIGIN);
+      context.response.headers.set('Access-Control-Allow-Methods', ACCESS_CONTROL_ALLOW_METHODS);
+      context.response.headers.set('Access-Control-Allow-Headers', ACCESS_CONTROL_ALLOW_HEADERS);
+      context.response.headers.set('Content-Security-Policy', CONTENT_SECURITY_POLICY);
+      context.response.headers.set('X-Content-Type-Options', X_CONTENT_TYPE_OPTIONS);
+      context.response.headers.set('X-Frame-Options', X_FRAME_OPTIONS);
+      context.response.headers.set('X-XSS-Protection', X_XSS_PROTECTION);
+      context.response.headers.set('Strict-Transport-Security', STRICT_TRANSPORT_SECURITY);
+      context.response.headers.set('Referrer-Policy', REFERRER_POLICY);
+      context.response.headers.set('Permissions-Policy', PERMISSIONS_POLICY);
 
       if (context.request.method === 'OPTIONS') {
         context.response.status = 204;
--- test/deps.ts
+++ test/deps.ts
@@ -1,5 +1,5 @@
 export { testing } from 'https://deno.land/x/oak@v12.6.1/mod.ts';
 export type { Middleware } from 'https://deno.land/x/oak@v12.6.1/mod.ts';
-export { assert, assertEquals, assertFalse, assertObjectMatch } from 'https://deno.land/std@0.204.0/assert/mod.ts';
+export { assert, assertEquals, assertFalse, assertObjectMatch } from 'https://deno.land/std@0.208.0/assert/mod.ts';
 export { superoak } from 'https://deno.land/x/superoak@4.7.0/mod.ts';
 export type { IResponse } from 'https://deno.land/x/superoak@4.7.0/mod.ts';
