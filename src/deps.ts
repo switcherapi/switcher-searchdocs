292899259c641ddeb7dc6de97a70f2d527422714
Roger Floriano <31597636+petruki@users.noreply.github.com>
2024-03-02 15:13:54 -0800

    Bump oak@14.1.1, skimming@1.0.12 (#22)

--- src/deps.ts
+++ src/deps.ts
@@ -1,7 +1,7 @@
-export { Application, Router } from 'https://deno.land/x/oak@v14.0.0/mod.ts';
-export type { Middleware, Next } from 'https://deno.land/x/oak@v14.0.0/mod.ts';
-export { Context } from 'https://deno.land/x/oak@v14.0.0/context.ts';
-export { load } from 'https://deno.land/std@0.217.0/dotenv/mod.ts';
-export { bold, cyan, green } from 'https://deno.land/std@0.217.0/fmt/colors.ts';
-export { Skimming } from 'https://deno.land/x/skimming@v1.0.11/mod.ts';
-export type { Output } from 'https://deno.land/x/skimming@v1.0.11/mod.ts';
+export { Application, Router } from 'https://deno.land/x/oak@v14.1.1/mod.ts';
+export type { Middleware, Next } from 'https://deno.land/x/oak@v14.1.1/mod.ts';
+export { Context } from 'https://deno.land/x/oak@v14.1.1/context.ts';
+export { load } from 'https://deno.land/std@0.218.2/dotenv/mod.ts';
+export { bold, cyan, green } from 'https://deno.land/std@0.218.2/fmt/colors.ts';
+export { Skimming } from 'https://deno.land/x/skimming@v1.0.12/mod.ts';
+export type { Output } from 'https://deno.land/x/skimming@v1.0.12/mod.ts';
