8fc0265614685d63645baad674ebf36435f4e1cf
Roger Floriano <31597636+petruki@users.noreply.github.com>
2024-06-25 18:25:35 -0700

    Bump Deno runtime to 1.44.4 - Oak@16.1.1, Validator4Oak@1.1.0 (#26)

--- test/deps.ts
+++ test/deps.ts
@@ -1,3 +1,3 @@
-export { type Middleware, testing } from 'jsr:@oak/oak@16.0.0';
+export { type Middleware, testing } from 'jsr:@oak/oak@16.1.0';
 export { assert, assertEquals, assertFalse, assertObjectMatch } from 'https://deno.land/std@0.224.0/assert/mod.ts';
 export { type IResponse, superoak } from 'https://deno.land/x/superoak@4.8.1/mod.ts';
