9f58cf9c2754c7e01fd40cc53f08afe6cb3d9ddb
petruki <31597636+petruki@users.noreply.github.com>
2024-06-25 18:23:56 -0700

    Bump Deno runtime to 1.44.4 - Oak@16.1.1, Validator4Oak@1.1.0

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,6 @@
-export { Application, Context, Router } from 'jsr:@oak/oak@16.0.0';
-export type { Middleware, Next } from 'jsr:@oak/oak@16.0.0';
+export { Application, Context, Router } from 'jsr:@oak/oak@16.1.0';
+export type { Middleware, Next } from 'jsr:@oak/oak@16.1.0';
 export { load } from 'https://deno.land/std@0.224.0/dotenv/mod.ts';
 export { bold, cyan, green } from 'https://deno.land/std@0.224.0/fmt/colors.ts';
 export { type Output, Skimming } from 'jsr:@trackerforce/skimming@1.2.1';
-export { ValidatorFn, ValidatorMiddleware } from 'jsr:@trackerforce/validator4oak@1.0.1';
+export { ValidatorFn, ValidatorMiddleware } from 'jsr:@trackerforce/validator4oak@1.1.0';
