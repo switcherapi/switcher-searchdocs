7b3fef07023e9b811e0d36586d4959e7db82aee4
Roger Floriano <31597636+petruki@users.noreply.github.com>
2024-09-14 22:36:51 -0700

    Bump Deno to 1.46.3, oak@17.0.0 (#28)

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,6 @@
-export { Application, Context, Router } from 'jsr:@oak/oak@16.1.0';
-export type { Middleware, Next } from 'jsr:@oak/oak@16.1.0';
-export { load } from 'https://deno.land/std@0.224.0/dotenv/mod.ts';
-export { bold, cyan, green } from 'https://deno.land/std@0.224.0/fmt/colors.ts';
+export { Application, Context, Router } from 'jsr:@oak/oak@17.0.0';
+export type { Middleware, Next } from 'jsr:@oak/oak@17.0.0';
+export { load } from 'jsr:@std/dotenv@0.225.2';
+export { bold, cyan, green } from 'jsr:@std/fmt@1.0.2/colors';
 export { type Output, Skimming } from 'jsr:@trackerforce/skimming@1.2.1';
 export { ValidatorFn, ValidatorMiddleware } from 'jsr:@trackerforce/validator4oak@1.1.0';
--- test/deps.ts
+++ test/deps.ts
@@ -1,3 +1,3 @@
-export { type Middleware, testing } from 'jsr:@oak/oak@16.1.0';
-export { assert, assertEquals, assertFalse, assertObjectMatch } from 'https://deno.land/std@0.224.0/assert/mod.ts';
+export { type Middleware, testing } from 'jsr:@oak/oak@17.0.0';
+export { assert, assertEquals, assertFalse, assertObjectMatch } from 'jsr:@std/assert@1.0.5';
 export { type IResponse, superoak } from 'https://deno.land/x/superoak@4.8.1/mod.ts';
