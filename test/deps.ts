67c501ddc121416f03d391d058d08ceae7f888b3
Roger Floriano <31597636+petruki@users.noreply.github.com>
2025-02-23 12:29:32 -0800

    Bump Deno runtime to 2.2.1 (#30)

--- test/deps.ts
+++ test/deps.ts
@@ -1,5 +1,5 @@
-export { type Middleware, testing } from 'jsr:@oak/oak@17.1.3';
-export { assert, assertEquals, assertFalse, assertObjectMatch } from 'jsr:@std/assert@1.0.9';
+export { type Middleware, testing } from 'jsr:@oak/oak@17.1.4';
+export { assert, assertEquals, assertFalse, assertObjectMatch } from 'jsr:@std/assert@1.0.11';
 export { type IResponse, superoak } from 'https://deno.land/x/superoak@4.8.1/mod.ts';
 
 // Fixes superdeno@4.9.0 issue
